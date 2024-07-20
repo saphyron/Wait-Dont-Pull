5840998d6b71bc21613282f9ff58f5f7100325ba
John Grandt Markvard Høeg <30288325+saphyron@users.noreply.github.com>
2024-07-20 13:59:02 +0000

    Updated this weeks data

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const App = () => {
   // Render the App component
   return (
     <div className="App">
-      <h1>Guild Roster (Updated 10:04 10/07/24)</h1>
+      <h1>Guild Roster (Updated 15:27 20/07/24)</h1>
       <table>
         <thead>
           <tr>
