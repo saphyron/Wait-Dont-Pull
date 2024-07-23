ee54487bf7f3bea900d6965b2bf544862a68e489
John Grandt Markvard Høeg <30288325+saphyron@users.noreply.github.com>
2024-07-23 07:31:03 +0000

    decayed guild stats and weekly update to data.json

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ const App = () => {
   // Render the App component
   return (
     <div className="App">
-      <h1>Guild Roster (Updated 15:27 20/07/24)</h1>
+      <h1>Guild Roster (Updated 09:30 23/07/24)</h1>
       <table>
         <thead>
           <tr>
